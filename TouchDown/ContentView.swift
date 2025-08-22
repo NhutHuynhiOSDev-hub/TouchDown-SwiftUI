4d16d4e674755effbc672958c8f18a05fa269431
nhuthq <nhuthq@zigexn.vn>
2025-08-22 13:13:05 +0700

    add resource

---
+++ TouchDown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TouchDown
+//
+//  Created by Nhut Huynh Quang on 22/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Nhut Huynh Quang on 22/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
